65a0a29ec2edd01d66ad5528b709c289753383d8
Gabriel <Gabriel@DESKTOP-SD7HQPS>
2021-09-17 14:42:10 -0300

    Add project files.

---
+++ src/Authentication.Infra.CrossCutting.IoC/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Authentication.Infra.CrossCutting.IoC
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/Authentication.Infra.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Authentication.Infra.Data
+{
+    public class Class1
+    {
+    }
+}
